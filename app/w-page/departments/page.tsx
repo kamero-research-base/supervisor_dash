b417c38224780f75b9311a0fc142ac37671444ac
CodeReveur <codereveur@gmail.com>
2025-02-08 21:05:12 +0200

    initial upload 1

--- app/w-page/departments/page.tsx
+++ app/w-page/departments/page.tsx
@@ -1,10 +1,11 @@
-"use client";
+"use client"
 import Header, { DepartmentList } from "@/app/components/departments";
 import AddDepartment from "@/app/components/toggles/addDepartment";
 import { useState } from "react";
 
 export default function Departments(){
   const [showAddDepartment, setShowAddDepartment] = useState(false);
+  const [setupDepartmentId, setSetupDepartmentId] = useState<number | null>(null);
 
   const toggleAddDepartment = () => {
     setShowAddDepartment(true);
@@ -12,10 +13,16 @@ export default function Departments(){
   const closeAddDepartment = () => {
     setShowAddDepartment(false);
   }
+  const handleDepartmentViewClick = (DepartmentId: number) => {
+    setSetupDepartmentId(DepartmentId); // Set the ID for the setup form
+  };
+  const closeDepartmentView = () => {
+    setSetupDepartmentId(null); // Close the setup product form
+  };
   return (
     <>
     <Header onAddDepartmentClick={toggleAddDepartment}/>
-    <DepartmentList />
+    <DepartmentList onDepartmentView={handleDepartmentViewClick} />
     {showAddDepartment && (
       <AddDepartment onClose={closeAddDepartment} />
     )}
